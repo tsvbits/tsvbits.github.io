a8b26356266fbeec0a71a8a9d43e325bf24ef846
Sergey Todyshev <stodyshev@gmail.com>
2020-12-29 16:08:25 +0700

    add link to CR profile

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,20 @@ import { Link } from 'gatsby';
 import github from 'simple-icons/icons/github';
 import linkedin from 'simple-icons/icons/linkedin';
 import twitter from 'simple-icons/icons/twitter';
+import CR from 'simple-icons/icons/codersrank';
 import { Icon } from '@iconify/react';
 import heartOutlined from '@iconify/icons-ant-design/heart-outlined';
 
 import { scale } from '../utils/typography';
 
-function SvgLink({ href, icon }) {
+function SvgLink({ href, icon, title }) {
   return (
     <a
       className="icon simple-icon"
       target="_blank"
       href={href}
       dangerouslySetInnerHTML={{ __html: icon.svg }}
+      title={title}
     ></a>
   );
 }
@@ -39,17 +41,32 @@ export default function Header() {
           <b>tsvbits</b>
         </Link>
       </h1>
-      <span className="internet-links" style={{ marginLeft: 50 }}>
-        <SvgLink href="https://www.github.com/sergeyt" icon={github} />
+      <span className="internet-links">
+        <SvgLink
+          href="https://profile.codersrank.io/user/sergeyt/"
+          icon={CR}
+          title="CodersRank Profile"
+        />
+        <SvgLink
+          href="https://www.github.com/sergeyt"
+          icon={github}
+          title="GitHub Profile"
+        />
         <SvgLink
           href="https://www.linkedin.com/in/sergeytodyshev"
           icon={linkedin}
+          title="LinkedIn Profile"
         />
-        <SvgLink href="https://www.twitter.com/todysh" icon={twitter} />
+        <SvgLink
+          href="https://www.twitter.com/todysh"
+          icon={twitter}
+          title="Twitter"
+        />
         <a
           className="icon"
           href="/sponsor"
           style={{ fontSize: 37, color: '#444' }}
+          title="Sponsor me"
         >
           <Icon icon={heartOutlined} />
         </a>
