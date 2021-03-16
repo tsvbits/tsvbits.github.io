1aa4663d8b866e411a747e8fbaa84136ee3f88c7
Sergey Todyshev <stodyshev@gmail.com>
2021-03-16 23:26:26 +0300

    update gatsby 3.0

--- gatsby-node.js
+++ gatsby-node.js
@@ -209,26 +209,3 @@ exports.onCreateNode = ({ node, actions }) => {
     });
   }
 };
-
-exports.onCreateWebpackConfig = ({
-  stage,
-  rules,
-  loaders,
-  plugins,
-  actions,
-}) => {
-  actions.setWebpackConfig({
-    // Some libraries import Node modules but don't use them in the browser.
-    // Tell webpack to provide empty mocks for them so importing them works.
-    node: {
-      module: 'empty',
-      dgram: 'empty',
-      dns: 'mock',
-      fs: 'empty',
-      http2: 'empty',
-      net: 'empty',
-      tls: 'empty',
-      child_process: 'empty',
-    },
-  });
-};
