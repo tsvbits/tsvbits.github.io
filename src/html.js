9eef3f98f201d90e97339fbd07df487c4434ac1b
Sergey Todyshev <stodyshev@gmail.com>
2021-04-26 09:20:55 +0300

    add yandex.metrica

--- src/html.js
+++ src/html.js
@@ -67,6 +67,33 @@ export default class HTML extends React.Component {
             `,
             }}
           />
+          {/* Yandex.Metrika counter */}
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(76572016, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+`,
+            }}
+          ></script>
+          <noscript>
+            <div>
+              <img
+                src="https://mc.yandex.ru/watch/76572016"
+                style={{ position: 'absolute', left: '-9999px' }}
+                alt=""
+              />
+            </div>
+          </noscript>
+          {/* /Yandex.Metrika counter */}
           {this.props.preBodyComponents}
           <div
             key={`body`}
