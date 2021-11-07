eece6e5da638037a19033006dbaf71d36eae1160
Sergey Todyshev <stodyshev@gmail.com>
2021-11-07 14:29:13 +0300

    cleanup unused Timeline

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -234,46 +234,6 @@ const Education = ({ education }) =>
     </div>
   );
 
-const Timeline = ({ isLast }) => {
-  return (
-    <>
-      <div
-        style={{
-          position: 'absolute',
-          width: 24,
-          height: 24,
-          border: '1px solid var(--textLink)',
-          borderRadius: '100%',
-          left: -30,
-          top: 3,
-          padding: 6,
-        }}
-      >
-        <div
-          style={{
-            width: 10,
-            height: 10,
-            backgroundColor: 'var(--textLink)',
-            borderRadius: '100%',
-          }}
-        ></div>
-      </div>
-      {isLast ? null : (
-        <div
-          style={{
-            position: 'absolute',
-            width: 2,
-            left: -19,
-            top: 26,
-            bottom: -31,
-            backgroundColor: 'var(--textLink)',
-          }}
-        ></div>
-      )}
-    </>
-  );
-};
-
 const Period = ({ startDate, endDate }) => (
   <b>
     <span>{formatPeriod(startDate)}</span>
